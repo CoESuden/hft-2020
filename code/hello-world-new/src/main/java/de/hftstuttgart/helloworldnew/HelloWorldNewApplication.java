1cd6a3fd25ef77e835c2419ef0d1b49beb41d8b7
Thanh Luong Giang <Thanh Luong Giang>
2020-10-30 09:08:28 +0100

    change requestMapping to getMapping, add PathVariable

--- code/hello-world-new/src/main/java/de/hftstuttgart/helloworldnew/HelloWorldNewApplication.java
+++ code/hello-world-new/src/main/java/de/hftstuttgart/helloworldnew/HelloWorldNewApplication.java
@@ -3,7 +3,7 @@ package de.hftstuttgart.helloworldnew;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,10 +16,9 @@ public class HelloWorldNewApplication {
 		return "Hello, Happy HfT Students!";
 	}
 
-	@RequestMapping("/test")
-	public String sayHelloFromOtherEndpoint(){
-
-		return "Hello, from the Request Endpoint!";
+	@GetMapping(value = "/test/{name}")
+	public String sayHelloFromOtherEndpoint(@PathVariable String name){
+		return "Hello, from " + name;
 	}
 
 	public static void main(String[] args) {

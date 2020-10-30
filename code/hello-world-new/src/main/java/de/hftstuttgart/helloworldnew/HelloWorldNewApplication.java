19d1c50b8603941e65095b0b7998c02a3584331c
Thanh Luong Giang <Thanh Luong Giang>
2020-10-30 09:43:48 +0100

    add arraylist putMapping and getMapping

--- code/hello-world-new/src/main/java/de/hftstuttgart/helloworldnew/HelloWorldNewApplication.java
+++ code/hello-world-new/src/main/java/de/hftstuttgart/helloworldnew/HelloWorldNewApplication.java
@@ -1,18 +1,23 @@
 package de.hftstuttgart.helloworldnew;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class HelloWorldNewApplication {
-
+	
+	private static Collection<String> _randomStrings = new ArrayList<>();
+	
 	@GetMapping("/")
 	public String sayHello(){
-
 		return "Hello, Happy HfT Students!";
 	}
 
@@ -20,9 +25,19 @@ public class HelloWorldNewApplication {
 	public String sayHelloFromOtherEndpoint(@PathVariable String name){
 		return "Hello, from " + name;
 	}
+	
+	@PutMapping("/myinput/{input}")
+	public Collection<String> addNewString(@PathVariable String input) {
+		_randomStrings.add(input);
+		return _randomStrings;
+	}
+	
+	@GetMapping("/myinput/getall")
+	public String getMyInput() {
+		return _randomStrings.toString();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldNewApplication.class, args);
 	}
-
 }

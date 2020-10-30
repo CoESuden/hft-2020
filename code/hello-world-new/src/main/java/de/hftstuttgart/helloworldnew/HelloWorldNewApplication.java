e220c9b3ff77ec9cb9793bd8a5a71d6c068bf8d2
Thanh Luong Giang <Thanh Luong Giang>
2020-10-30 09:50:40 +0100

    add logger and log /test/{name}

--- code/hello-world-new/src/main/java/de/hftstuttgart/helloworldnew/HelloWorldNewApplication.java
+++ code/hello-world-new/src/main/java/de/hftstuttgart/helloworldnew/HelloWorldNewApplication.java
@@ -3,6 +3,8 @@ package de.hftstuttgart.helloworldnew;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ public class HelloWorldNewApplication {
 	
 	private static Collection<String> _randomStrings = new ArrayList<>();
 	
+	private Logger _logger =  LoggerFactory.getLogger(HelloWorldNewApplication.class);
+	
 	@GetMapping("/")
 	public String sayHello(){
 		return "Hello, Happy HfT Students!";
@@ -23,6 +27,7 @@ public class HelloWorldNewApplication {
 
 	@GetMapping("/test/{name}")
 	public String sayHelloFromOtherEndpoint(@PathVariable String name){
+		_logger.info("This request has been called");
 		return "Hello, from " + name;
 	}
 	
